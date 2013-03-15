f3574b722520c606655324ebdfdfbcbb5ebb927a
Marco Costa <1110206@isep.ipp.pt>
2013-03-15 23:27:20 +0000

    Remocao de comentarios

--- EAPLI/src/eapli/EAPLI.java
+++ EAPLI/src/eapli/EAPLI.java
@@ -1,23 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package eapli;
 
-/**
- *
- * @author Marco
- */
 public class EAPLI {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
-    
-    //Marco Costa
     
     
     
